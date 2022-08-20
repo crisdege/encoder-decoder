f9f92a8dfb22b57d4306cafe6e5e3b7acd091b6f
Cristina Dege <cris.dege0813@gmail.com>
2022-08-20 11:08:22 -0400

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,79 @@
+// var word = "Hello World";
+var word = document.getElementById("wordToEncode").value;
+
+const mapWord = (word) => {
+  const mappedArray = [];
+  word
+    // .trim()
+    .toLowerCase()
+    .split("")
+    .forEach((char) => {
+      const ascii = char.charCodeAt();
+      if (ascii >= 97 && ascii <= 122) {
+        mappedArray.push(ascii - 96);
+      }
+    });
+  // return mappedArray;
+
+  //math array========================================================
+  var mathArray = [];
+  for (i = 0; i < mappedArray.length; i++) {
+    if (i === 0) {
+      num = mappedArray[0] + mappedArray[0 + 1];
+    } else if (i === mappedArray.length - 1) {
+      num = mappedArray[i] + mappedArray[i - 1];
+    } else {
+      num = mappedArray[i] + mappedArray[i - 1] + mappedArray[i + 1];
+    }
+    mathArray.push(num);
+  }
+  // return mathArray;
+  //====================================================================
+  //math for changing math so that numbers can be changed into letters
+  var encodedArray = [];
+  for (i = 0; i < mathArray.length; i++) {
+    if (mathArray[i] > 75) {
+      num = mathArray[i] - 75;
+    } else if (mathArray[i] > 50 && mathArray[i] < 75) {
+      num = mathArray[i] - 50;
+    } else if (mathArray[i] > 25 && mathArray[i] < 50) {
+      num = mathArray[i] - 25;
+    } else {
+      num = mathArray[i];
+    }
+    encodedArray.push(num);
+  }
+  // return encodedArray;
+  //breaks down the big array into a small array----------------------
+  // var shortArrays = [],
+  //   i,
+  //   len;
+
+  // for (i = 0, len = mappedArray.length; i < len; i += 3) {
+  //   shortArrays.push(mappedArray.slice(i, i + 3));
+  // }
+
+  // for (i = 0, len = shortArrays.length; i < len; i++) {
+  //   return shortArrays;
+  // }
+  //-----------------------------------------------------------------
+
+  //changes number array into corresponding character-----------------
+  const answer = encodedArray
+    .toString()
+    .split(",")
+    .map((x) => +x + 96)
+    .map(String.fromCharCode)
+    .join("");
+  return answer;
+  //-----------------------------------------------------------------
+};
+
+function encodeFunction() {
+  let word = document.getElementById("wordToEncode").value;
+
+  document.getElementById("demo").innerHTML = mapWord(word);
+  console.log(mapWord(word));
+}
+
+encodeFunction();
